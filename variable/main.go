4964b2e38f32c5e1dcc58970f12c48ef28305cf6
saud maruli <uthrul@gmail.com>
2018-11-21 15:09:16 +0700

    variable

---
+++ variable/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	m := "hasilnya adalah sebagai berikut "
+
+	var x int
+	x = 10
+
+	y := 10
+
+	var z int
+	z = x * y
+
+	fmt.Println(m, x, y, z)
+}
